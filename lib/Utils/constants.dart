907f951f7ae60b0d98a888b19359dfb1dea064b5
이가영 <dlrk2258@gmail.com>
2023-02-06 17:54:55 +0900

    ui update

--- lib/Utils/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xfff9f5fa);
-const kBackgroundColor = Color(0xfff1ebf2);
-const kProfileColor = Color(0xffebe1ed);
-const kIconColor = Color(0xffb138e0);
-const kButtonColor= Color(0xffbc67c9);
