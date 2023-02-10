57ad5a1c407dd326629d845b00f09897377d8b24
이가영 <dlrk2258@gmail.com>
2023-02-10 16:41:21 +0900

    background color 변경

--- lib/Utils/constants.dart
+++ lib/Utils/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xfff9f5fa);
-const kBackgroundColor = Color(0xfff1ebf2);
-const kProfileColor = Color(0xffebe1ed);
+const kPrimaryColor = Color(0xffFAF3EC);
+const kBackgroundColor = Color(0xffFAF3EC);
+const kProfileColor = Color(0xffFAF3EC);
 const kIconColor = Color(0xfff76707);
 const kButtonColor= Color(0xfff76707);
